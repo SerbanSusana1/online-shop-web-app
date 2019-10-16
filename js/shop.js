c01e37553327a3d725b289dd9e8ed04937a18baa
susana.serban <susana.serban@casomes.ro>
2019-10-16 20:02:17 +0300

    adding product to cart and displaying cart content  from api

--- js/shop.js
+++ js/shop.js
@@ -39,7 +39,43 @@ window.Shop = {
                     </div>
                 </div>`
 
+    },
+
+    addProductToCart(productId) {
+        // customerId to be read from memory somehow in the future
+       var customerId =10;
+
+        var requestBody = {
+            customerId: customerId,
+            productId: productId
+        };
+
+        $.ajax({
+            url: Shop.API_BASE_URL + "/carts",
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify(requestBody)
+        }).done(function () {
+            window.location.replace("cart.html")
+
+        })
+    },
+
+
+
+    bindEvents: function () {
+        $(".single-product-area .row").delegate(".add_to_cart_button", "click", function (event) {
+           event.preventDefault();
+           //console.log("test");
+            var productId = $(this).data("product_id");
+            Shop.addProductToCart(productId);
+
+        })
+
     }
+
+
 };
 
 Shop.getProducts();
+Shop.bindEvents();
